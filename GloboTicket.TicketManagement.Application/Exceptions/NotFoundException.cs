9a865fe159ca852ad4db041c295bde6e31cd8af4
AmmarMKamel <ammarkamel9899@gmail.com>
2026-04-22 01:30:51 +0200

    feat: add custom exception classes for application errors

---
+++ GloboTicket.TicketManagement.Application/Exceptions/NotFoundException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GloboTicket.TicketManagement.Application.Exceptions
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException(string name, object key) : base($"{name} ({key}) is not found")
+        { }
+    }
+}
---
+++ GloboTicket.TicketManagement.Application/Exceptions/ValidationException.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GloboTicket.TicketManagement.Application.Exceptions
+{
+    public class ValidationException : Exception
+    {
+        public List<string> ValidationErrors { get; set; }
+
+        public ValidationException(ValidationResult validationResult)
+        {
+            ValidationErrors = new List<string>();
+
+            foreach (var validationError in validationResult.Errors)
+            {
+                ValidationErrors.Add(validationError.ErrorMessage);
+            }
+        }
+    }
+}
