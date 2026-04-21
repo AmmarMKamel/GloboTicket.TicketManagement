d16378de8cf50f3c209f45b4cce762a29ae3c1b8
AmmarMKamel <ammarkamel9899@gmail.com>
2026-04-21 22:12:28 +0200

    feat: add ApplicationServiceRegistration for DI setup

---
+++ GloboTicket.TicketManagement.Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GloboTicket.TicketManagement.Application
+{
+    public static class ApplicationServiceRegistration
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddAutoMapper(configAction =>
+            {
+                configAction.AddMaps(AppDomain.CurrentDomain.GetAssemblies());
+            });
+            services.AddMediatR(configuration =>
+            {
+                configuration.RegisterServicesFromAssemblies(AppDomain.CurrentDomain.GetAssemblies());
+            });
+
+            return services;
+        }
+    }
+}
