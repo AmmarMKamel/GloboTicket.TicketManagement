b2764ecb7ee8598a888e7c87631950d81fd5aca1
AmmarMKamel <ammarkamel9899@gmail.com>
2026-04-22 22:26:09 +0200

    feat: add repository implementations for Category, Event, Order

---
+++ GloboTicket.TicketManagement.Persistence/Repositories/EventRepository.cs
@@ -0,0 +1,21 @@
+﻿using GloboTicket.TicketManagement.Application.Contracts.Persistence;
+using GloboTicket.TicketManagement.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GloboTicket.TicketManagement.Persistence.Repositories
+{
+    public class EventRepository : BaseRepository<Event>, IEventRepository
+    {
+        public EventRepository(GloboTicketDbContext dbContext) : base(dbContext)
+        { }
+
+        public async Task<bool> IsEventNameAndDateUnique(string name, DateTime eventDate)
+        {
+            return await _dbContext.Events.AnyAsync(e => e.Name.Equals(name) &&
+                e.Date.Date.Equals(eventDate.Date));
+        }
+    }
+}
---
+++ GloboTicket.TicketManagement.Persistence/Repositories/OrderRepository.cs
@@ -0,0 +1,14 @@
+﻿using GloboTicket.TicketManagement.Application.Contracts.Persistence;
+using GloboTicket.TicketManagement.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GloboTicket.TicketManagement.Persistence.Repositories
+{
+    public class OrderRepository : BaseRepository<Order>, IOrderRepository
+    {
+        public OrderRepository(GloboTicketDbContext dbContext) : base(dbContext)
+        { }
+    }
+}
