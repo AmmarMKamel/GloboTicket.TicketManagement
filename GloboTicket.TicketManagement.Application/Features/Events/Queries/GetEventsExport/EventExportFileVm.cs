f32d632a1c2bc259d66fc010cf2e5cf45a0ddc2e
AmmarMKamel <ammarkamel9899@gmail.com>
2026-04-24 04:55:05 +0300

    feat: add event export to CSV feature with MediatR support

---
+++ GloboTicket.TicketManagement.Application/Features/Events/Queries/GetEventsExport/EventExportFileVm.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GloboTicket.TicketManagement.Application.Features.Events.Queries.GetEventsExport
+{
+    public class EventExportFileVm
+    {
+        public string EventExportFileName { get; set; } = string.Empty;
+        public string ContentType { get; set; } = string.Empty;
+        public byte[]? Data { get; set; }
+    }
+}
---
+++ GloboTicket.TicketManagement.Application/Features/Events/Queries/GetEventsExport/GetEventsExportQueryHandler.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using GloboTicket.TicketManagement.Application.Contracts.Infrastructure;
+using GloboTicket.TicketManagement.Application.Contracts.Persistence;
+using GloboTicket.TicketManagement.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GloboTicket.TicketManagement.Application.Features.Events.Queries.GetEventsExport
+{
+    public class GetEventsExportQueryHandler : IRequestHandler<GetEventsExportQuery, EventExportFileVm>
+    {
+        private readonly IAsyncRepository<Event> _eventRepository;
+        private readonly IMapper _mapper;
+        private readonly ICsvExporter _csvExporter;
+
+        public GetEventsExportQueryHandler(
+            IAsyncRepository<Event> eventRepository,
+            IMapper mapper,
+            ICsvExporter csvExporter)
+        {
+            _eventRepository = eventRepository ?? throw new ArgumentNullException(nameof(eventRepository));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+            _csvExporter = csvExporter ?? throw new ArgumentNullException(nameof(csvExporter));
+        }
+
+        public async Task<EventExportFileVm> Handle(GetEventsExportQuery request, CancellationToken cancellationToken)
+        {
+            var allEvents = _mapper.Map<List<EventExportDto>>(
+                (await _eventRepository.ListAllAsync()).OrderBy(e => e.Date));
+
+            var fileData = _csvExporter.ExportEventsToCsv(allEvents);
+
+            var eventExportFileDto = new EventExportFileVm()
+            {
+                ContentType = "text/csv",
+                Data = fileData,
+                EventExportFileName = $"{Guid.NewGuid()}.csv"
+            };
+
+            return eventExportFileDto;
+        }
+    }
+}
