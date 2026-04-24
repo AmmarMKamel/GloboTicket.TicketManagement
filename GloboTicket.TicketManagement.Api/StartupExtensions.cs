a8ef6760cf8608b396533ec3e8fff7ae0dcbae7e
AmmarMKamel <ammarkamel9899@gmail.com>
2026-04-24 04:07:56 +0300

    feat: add ASP.NET Core Web API project and configuration

---
+++ GloboTicket.TicketManagement.Api/StartupExtensions.cs
@@ -0,0 +1,63 @@
+﻿using GloboTicket.TicketManagement.Application;
+using GloboTicket.TicketManagement.Infrastructure;
+using GloboTicket.TicketManagement.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace GloboTicket.TicketManagement.Api
+{
+    public static class StartupExtensions
+    {
+        public static WebApplication ConfigureServices(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddApplicationServices();
+            builder.Services.AddPersistenceServices(builder.Configuration);
+            builder.Services.AddInfrastructureServices(builder.Configuration);
+
+            builder.Services.AddControllers();
+
+            builder.Services.AddCors(options =>
+                options.AddPolicy(
+                    "open",
+                    policy =>
+                    {
+                        policy.WithOrigins([
+                            builder.Configuration["ApiUrl"] ?? "https://localhost:7211",
+                            builder.Configuration["BlazorUrl"] ?? "https://localhost:7212"
+                        ])
+                        .AllowAnyMethod()
+                        .SetIsOriginAllowed(policy => true)
+                        .AllowAnyHeader()
+                        .AllowCredentials();
+                    }));
+
+            return builder.Build();
+        }
+
+        public static WebApplication ConfigurePipeline(this WebApplication app)
+        {
+            app.UseCors("open");
+            app.UseHttpsRedirection();
+            app.MapControllers();
+
+            return app;
+        }
+
+        public static async Task ResetDatabaseAsync(this WebApplication app)
+        {
+            using var scope = app.Services.CreateScope();
+            try
+            {
+                var context = scope.ServiceProvider.GetService<GloboTicketDbContext>();
+                if (context != null)
+                {
+                    await context.Database.EnsureDeletedAsync();
+                    await context.Database.MigrateAsync();
+                }
+            }
+            catch (Exception)
+            {
+                // Handle exceptions if needed
+            }
+        }
+    }
+}
