c508e21a970d76b532c2c80c5e67fc350483b635
AmmarMKamel <ammarkamel9899@gmail.com>
2026-04-25 01:53:52 +0300

    feat: add LoggedInUserService and ILoggedInUserService interface

---
+++ GloboTicket.TicketManagement.Api/Services/LoggedInUserService.cs
@@ -0,0 +1,23 @@
+﻿using GloboTicket.TicketManagement.Application.Contracts;
+using System.Security.Claims;
+
+namespace GloboTicket.TicketManagement.Api.Services
+{
+    public class LoggedInUserService : ILoggedInUserService
+    {
+        private readonly IHttpContextAccessor _contextAccessor;
+
+        public LoggedInUserService(IHttpContextAccessor contextAccessor)
+        {
+            _contextAccessor = contextAccessor ?? throw new ArgumentNullException(nameof(contextAccessor));
+        }
+
+        public string UserId
+        {
+            get
+            {
+                return _contextAccessor.HttpContext?.User?.FindFirstValue(ClaimTypes.NameIdentifier);
+            }
+        }
+    }
+}
