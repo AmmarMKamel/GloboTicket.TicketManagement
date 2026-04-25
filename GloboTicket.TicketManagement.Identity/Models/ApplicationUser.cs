2848e9a12674c5a8c3fe141baf818dfb0d2ab775
AmmarMKamel <ammarkamel9899@gmail.com>
2026-04-26 00:13:33 +0300

    feat: add Identity project with custom user and service setup

---
+++ GloboTicket.TicketManagement.Identity/Models/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GloboTicket.TicketManagement.Identity.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
