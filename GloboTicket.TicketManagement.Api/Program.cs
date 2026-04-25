c24300f46f5070aef4ce2af2eb58e76fe148ed33
AmmarMKamel <ammarkamel9899@gmail.com>
2026-04-25 23:28:31 +0300

    feat: ntegrate Serilog for structured logging

--- GloboTicket.TicketManagement.Api/Program.cs
+++ GloboTicket.TicketManagement.Api/Program.cs
@@ -1,9 +1,25 @@
 using GloboTicket.TicketManagement.Api;
+using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog((context, services, configuration) =>
+    configuration
+        .ReadFrom.Configuration(context.Configuration)
+        .ReadFrom.Services(services)
+        .Enrich.FromLogContext()
+        .WriteTo.Console(),
+    true);
+
 var app = builder
     .ConfigureServices()
     .ConfigurePipeline();
+
+app.UseSerilogRequestLogging();
 
 if (!app.Environment.IsEnvironment("Testing"))
 {
