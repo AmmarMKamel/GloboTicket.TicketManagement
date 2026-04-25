fe53326a2db4b54b8b72853811b028063508d9a0
AmmarMKamel <ammarkamel9899@gmail.com>
2026-04-26 00:13:58 +0300

    feat: add Identity integration and endpoints to API

--- GloboTicket.TicketManagement.Api/StartupExtensions.cs
+++ GloboTicket.TicketManagement.Api/StartupExtensions.cs
@@ -1,9 +1,13 @@
 ﻿using GloboTicket.TicketManagement.Api.Services;
 using GloboTicket.TicketManagement.Application;
 using GloboTicket.TicketManagement.Application.Contracts;
+using GloboTicket.TicketManagement.Identity;
+using GloboTicket.TicketManagement.Identity.Models;
 using GloboTicket.TicketManagement.Infrastructure;
 using GloboTicket.TicketManagement.Persistence;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
 
 namespace GloboTicket.TicketManagement.Api
 {
@@ -14,6 +18,7 @@ namespace GloboTicket.TicketManagement.Api
             builder.Services.AddApplicationServices();
             builder.Services.AddPersistenceServices(builder.Configuration);
             builder.Services.AddInfrastructureServices(builder.Configuration);
+            builder.Services.AddIdentityServices(builder.Configuration);
 
             builder.Services.AddScoped<ILoggedInUserService, LoggedInUserService>();
 
@@ -36,6 +41,7 @@ namespace GloboTicket.TicketManagement.Api
                         .AllowCredentials();
                     }));
 
+            builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
             return builder.Build();
@@ -43,6 +49,14 @@ namespace GloboTicket.TicketManagement.Api
 
         public static WebApplication ConfigurePipeline(this WebApplication app)
         {
+            app.MapIdentityApi<ApplicationUser>();
+
+            app.MapPost("/Logout", async (ClaimsPrincipal user, SignInManager<ApplicationUser> signInManager) =>
+            {
+                await signInManager.SignOutAsync();
+                return TypedResults.Ok();
+            });
+
             app.UseCors("open");
 
             if (app.Environment.IsDevelopment())
