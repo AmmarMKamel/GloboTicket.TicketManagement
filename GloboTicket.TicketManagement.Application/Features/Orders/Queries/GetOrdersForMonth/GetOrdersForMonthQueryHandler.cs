504ba6e8b034adb1ad1ad08613b74111bf55df0b
AmmarMKamel <ammarkamel9899@gmail.com>
2026-04-25 22:31:42 +0300

    feat: add paged orders by month query and related DTOs

---
+++ GloboTicket.TicketManagement.Application/Features/Orders/Queries/GetOrdersForMonth/GetOrdersForMonthQueryHandler.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using GloboTicket.TicketManagement.Application.Contracts.Persistence;
+using MediatR;
+
+namespace GloboTicket.TicketManagement.Application.Features.Orders.Queries.GetOrdersForMonth
+{
+    internal class GetOrdersForMonthQueryHandler
+        : IRequestHandler<GetOrdersForMonthQuery, PagedOrdersForMonthVm>
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly IMapper _mapper;
+
+        public GetOrdersForMonthQueryHandler(IOrderRepository orderRepository, IMapper mapper)
+        {
+            _orderRepository = orderRepository ?? throw new ArgumentNullException(nameof(orderRepository));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        }
+
+        public async Task<PagedOrdersForMonthVm> Handle(
+            GetOrdersForMonthQuery request, CancellationToken cancellationToken)
+        {
+            var list = await _orderRepository.GetPagedOrdersForMonth(request.Date, request.Page, request.Size);
+            var orders = _mapper.Map<List<OrdersForMonthDto>>(list);
+
+            var count = await _orderRepository.GetTotalCountOfOrdersForMonth(request.Date);
+
+            return new PagedOrdersForMonthVm()
+            {
+                Count = count,
+                Page = request.Page,
+                Size = request.Size,
+                OrdersForMonth = orders
+            };
+        }
+    }
+}
